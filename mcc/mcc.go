856ebe0e54e93cf2383bc42d1563d274e0afaa15
Jeremy Summers <jez@wizard.technology>
2016-09-15 23:19:02 +0100

    Twitter and GPlus auth working

--- mcc/mcc.go
+++ mcc/mcc.go
@@ -27,6 +27,7 @@ type Page struct {
 	Title        string
 	Providers    []string
 	ProvidersMap map[string]string
+	User         goth.User
 }
 
 func init() {
@@ -40,10 +41,9 @@ func display(w http.ResponseWriter, tmpl string, data interface{}) {
 
 // SEE: https://www.socketloop.com/tutorials/golang-gorilla-mux-routing-example
 func main() {
-	fmt.Printf("%s", config)
 	goth.UseProviders(
-		twitter.New(os.Getenv("TWITTER_KEY"), os.Getenv("TWITTER_SECRET"), "http://www.mycrohnscolitis.org/auth/twitter/callback"),
-		gplus.New(os.Getenv("GPLUS_KEY"), os.Getenv("GPLUS_SECRET"), "http://www.mycrohnscolitis.org/auth/gplus/callback"), //https://console.developers.google.com/apis/credentials/wizard?api=plus-json.googleapis.com&project=mycrohnscolitis&authuser=1
+		twitter.New(os.Getenv("TWITTER_KEY"), os.Getenv("TWITTER_SECRET"), "http://www.mycrohnscolitis.org:8080/auth/twitter/callback"),
+		gplus.New(os.Getenv("GPLUS_KEY"), os.Getenv("GPLUS_SECRET"), "http://www.mycrohnscolitis.org:8080/auth/gplus/callback"), //https://console.developers.google.com/apis/credentials/wizard?api=plus-json.googleapis.com&project=mycrohnscolitis&authuser=1
 	)
 	m := make(map[string]string)
 	m["twitter"] = "Twitter"
@@ -57,16 +57,18 @@ func main() {
 
 	p := pat.New()
 
-	// p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
+	p.Get("/auth/{provider}/callback", func(w http.ResponseWriter, r *http.Request) {
 
-	// 	user, err := gothic.CompleteUserAuth(res, req)
-	// 	if err != nil {
-	// 		fmt.Fprintln(res, err)
-	// 		return
-	// 	}
-	// 	t, _ := template.New("foo").Parse(userTemplate)
-	// 	t.Execute(res, user)
-	// })
+		user, err := gothic.CompleteUserAuth(w, r)
+		if err != nil {
+			fmt.Fprintln(w, err)
+			return
+		}
+		//t, _ := template.New("foo").Parse(userTemplate)
+		//t.Execute(w, user)
+		fmt.Printf("User: %#v", user) //all the information is stored in $user
+		display(w, "user", &Page{Title: "User Page", User: user})
+	})
 
 	p.Get("/auth/{provider}", gothic.BeginAuthHandler)
 	p.Get("/login", func(w http.ResponseWriter, r *http.Request) {
